4f121c4718b7e743b1a53b0c7ec18ea3901d9d07
Power Gunn <151195459+powergunn@users.noreply.github.com>
2023-12-07 16:05:39 +0800

    Add files via upload

---
+++ squid_proxy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install Squid
+sudo apt -y install squid
+
+# Edit Squid configuration file and add configuration
+sudo sed -i '22i\dns_v4_first on' /etc/squid/squid.conf
+sudo sed -i '23i\http_access allow all' /etc/squid/squid.conf
+
+# Restart Squid service
+sudo systemctl restart squid
+
+# Enable Squid service to start on boot
+sudo systemctl enable squid
+
+# Install UFW (Uncomplicated Firewall)
+sudo apt -y install ufw
+
+# Allow traffic on port 3128
+sudo ufw allow 3128/tcp
+
+echo "Setup completed successfully."
